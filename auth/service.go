6ba14a20ce0689f392f4cc41a9621edb054910e9
akbarismail <ismailakbar.me@gmail.com>
2023-06-08 14:34:37 +0700

    jwt auth. create validation token

--- auth/service.go
+++ auth/service.go
@@ -1,11 +1,14 @@
 package auth
 
 import (
+	"errors"
+
 	"github.com/golang-jwt/jwt/v4"
 )
 
 type Service interface {
 	GenerateToke(userId int) (string, error)
+	ValidateToken(encodeToken string) (*jwt.Token, error)
 }
 
 type jwtService struct{}
@@ -28,4 +31,22 @@ func (j *jwtService) GenerateToke(userId int) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+func (j *jwtService) ValidateToken(encodeToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodeToken, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("invalid token")
+		}
+
+		return []byte(jwtSecretKey), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
